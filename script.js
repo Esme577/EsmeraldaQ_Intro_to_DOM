18aeacedf10f78aa13d27a743633d48037f983ea
Esme577 <esmeraldaquezada57@gmail.com>
2025-04-01 19:16:34 -0700

    what we do is that in html file we  give it a heading class or id and make sure we link our script file and in our script file we  we give a let and name it and then we set query slector and specify what we want it to go in and sortt of pull

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-let titleElement = document.getElementById("title");
+//getElementBYID
+//let titleElement = document.getElementById("title");
 
-console.log(titleElement);
+//console.log(titleElement);
+
+Let mainHeading = document.querySelector("#main-heading")
+Console.log(mainHeading)
+
+Let descparagraph = document.querySelector(".description")
+Console.log(descparagraph)
+
+Let header = document.querySelector("#h2")
+Console.log(header)
