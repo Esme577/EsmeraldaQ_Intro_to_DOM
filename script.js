5d3100dadad951b0a17da7fa224caee4fdc4c61b
Esme577 <esmeraldaquezada57@gmail.com>
2025-04-01 21:00:45 -0700

    I just changed my previous script a bit gave it a variable  and = it to document and .querySelectAll to grab all classes or p or h in my html and in thin in the () in quotes put the class or h or p that we want it to search for and console log and our variable name so that it brings it for us to see when we inspect

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-//getElementBYID
+//getElementBYID demo
 //let titleElement = document.getElementById("title");
 
 //console.log(titleElement);
 
-let para = document.querySelector("#mainheading");
+//Query selecor demo
+//let para = document.querySelector("#mainheading");
+//console.log(para);
+
+//let descParagraph = document.querySelector(".description");
+//console.log(descParagraph);
+
+//let header = document.querySelector("h2");
+//console.log(header);
+
+//Query selectorALL demo
+let para = document.querySelectorAll("#mainheading");
 console.log(para);
 
-let descParagraph = document.querySelector(".description");
+
+let descParagraph = document.querySelectorAll(".description");
 console.log(descParagraph);
 
-let header = document.querySelector("h2");
+let header = document.querySelectorAll("h2");
 console.log(header);
+//apend
+//let paragraph = document.createElement("p");
+
+//let body = document.body; 
+
+//console.log(body);
+
+//body.append(paragraph) 
